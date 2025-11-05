395cb0d246698bb5bbd65de8515be8d58dbb5e0f
shivnilay1516 <shivnilay.srivastav@ens.enterprises>
2025-11-05 18:39:38 +0530

    some ui modify

--- src/app/slipPage/SlipPage.tsx
+++ src/app/slipPage/SlipPage.tsx
@@ -38,66 +38,63 @@ export default function MyPayPage() {
   };
 
   return (
-    <div className="min-h-screen bg-[#0f172a] text-white flex flex-col md:flex-row">
-      <main className="flex-1 px-3 pb-6 space-y-4">
-        <h1 className="text-[28px] font-bold text-white">Pay Slips</h1>
-
-        <div className="flex gap-3 items-center">
-          <h2 className="text-lg font-semibold">Select Month and Year -</h2>
-          <div className="flex gap-3">
-            <div className="w-full relative">
-              <select
-                value={month}
-                onChange={handleMonthChange}
-                className="w-full text-white border border-gray-500 p-2 rounded bg-gray-800 appearance-none pr-8"
-              >
-                {months.map((m, i) => (
-                  <option key={i} value={i + 1}>
-                    {m}
-                  </option>
-                ))}
-              </select>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute right-2 top-[50%] -translate-y-1/2 w-4 h-4 text-white pointer-events-none"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </div>
-            <div className="w-full relative">
-              <input
-                type="number"
-                value={year}
-                onChange={handleYearInputChange}
-                className="w-full text-white border border-gray-500 p-2 rounded bg-gray-800 appearance-none pr-8"
+    <div className="min-h-screen bg-[#0f172a] text-white w-full">
+      <h1 className="text-[28px] font-bold text-white">Pay Slips</h1>
+      <div className="flex gap-3 items-center flex-wrap">
+        <h2 className="text-lg font-semibold">Select Month and Year -</h2>
+        <div className="flex gap-3">
+          <div className="w-full relative">
+            <select
+              value={month}
+              onChange={handleMonthChange}
+              className="w-full text-white border border-gray-500 p-2 rounded bg-gray-800 appearance-none pr-8"
+            >
+              {months.map((m, i) => (
+                <option key={i} value={i + 1}>
+                  {m}
+                </option>
+              ))}
+            </select>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute right-2 top-[50%] -translate-y-1/2 w-4 h-4 text-white pointer-events-none"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
               />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute right-2 top-[50%] -translate-y-1/2 w-4 h-4 text-white pointer-events-none"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </div>
+            </svg>
+          </div>
+          <div className="w-full relative">
+            <input
+              type="number"
+              value={year}
+              onChange={handleYearInputChange}
+              className="w-full text-white border border-gray-500 p-2 rounded bg-gray-800 appearance-none pr-8"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute right-2 top-[50%] -translate-y-1/2 w-4 h-4 text-white pointer-events-none"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
           </div>
         </div>
-        <PayslipPage month={month} year={year} />
-      </main>
+      </div>
+      <PayslipPage month={month} year={year} />
     </div>
   );
 }
